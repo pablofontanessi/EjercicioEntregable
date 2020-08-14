01f6afe8b45d13d9b896ff371f91a2d59aafebf9
pablofontanessi <44381282+pablofontanessi@users.noreply.github.com>
2020-08-14 19:45:04 -0300

    Ejercicio

---
+++ EjercicioEntregable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logica;
+
+namespace EjercicioEntregable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logica.Principal IniciarPrincipal = new Principal();
+            Console.WriteLine("Ingrese una opcion. 1 para obtener la antiguedad de un empleado, 2 para calcular el sueldo neto de un empleado, ");
+
+        }
+    }
+}
